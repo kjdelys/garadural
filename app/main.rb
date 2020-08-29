30168c23a96dce95f1d0f2ade5893ec86ba009d2
Kévin Joannon <kevin.joannon@outlook.fr>
2020-08-29 20:42:12 +0200

    Add visible method

--- app/main.rb
+++ app/main.rb
@@ -4,7 +4,7 @@ STEP_SIZE = 10
 def tick args
 
   args.outputs.labels << [10, 30, args.gtk.current_framerate]
-
+  
   #initialize screen
   #-map
   args.outputs.solids << [0, 0, 500, 500, 0, 0, 255, 128]
@@ -12,7 +12,7 @@ def tick args
   args.state.player = Character.new(50, 50, 500, 50, "sprites/icon.png") if args.state.player.nil?
   player = args.state.player
   args.outputs.sprites << player.image
-
+  args.outputs.labels << [500, 30, player.pos_x.to_s + " " + player.pos_y.to_s]
 
   #move player
   args.state.player.move_x(-STEP_SIZE) if args.inputs.keyboard.left
@@ -20,6 +20,7 @@ def tick args
   args.state.player.move_y(STEP_SIZE) if args.inputs.keyboard.up
   args.state.player.move_y(-STEP_SIZE) if args.inputs.keyboard.down
 
+  puts player.is_inside_visible?
 
   # if args.inputs.keyboard.key_down.left
   #   args.outputs.labels  << [500, 580, "pd", 4, 0, 88, 41, 0, 255, 'manaspc.ttf']
